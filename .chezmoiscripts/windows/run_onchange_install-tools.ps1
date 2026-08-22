636f5aa70dab6d9613ab1ce41f6fced1748d8cca
mkolibaba <avierdaner@gmail.com>
2026-08-22 23:20:18 +0300

    Separate VS Code extensions

--- .chezmoiscripts/windows/run_onchange_install-tools.ps1
+++ .chezmoiscripts/windows/run_onchange_install-tools.ps1
@@ -1,4 +1,4 @@
-$requiredTools = @(
+﻿$requiredTools = @(
     @{Name = "mise"; Package = "jdx.mise"}
     @{Name = "oh-my-posh"; Package = "JanDeDobbeleer.OhMyPosh"}
     @{Name = "zoxide"; Package = "ajeetdsouza.zoxide"}
@@ -21,42 +21,5 @@ foreach ($tool in $requiredTools) {
         }
     } else {
         Write-Host "✅ $($tool.Name) is already installed" -ForegroundColor Green
-    }
-}
-
-# VS Code extensions
-$vsCodeExtensions = @(
-    "andrewbutson.vscode-openapi-viewer",
-    "bcwsea.theme-saga",
-    "beardedbear.beardedicons",
-    "davidmarek.jsonpath-extract",
-    "docker.docker",
-    "dracula-theme.theme-dracula",
-    "editorconfig.editorconfig",
-    "fabianreyes.smart-json-schema",
-    "github.github-vscode-theme",
-    "golang.go",
-    "grapecity.gc-excelviewer",
-    "k--kato.intellij-idea-keybindings",
-    "maattdd.gitless",
-    "pkief.material-icon-theme",
-    "redhat.vscode-xml",
-    "redhat.vscode-yaml",
-    "richie5um2.vscode-statusbar-json-path",
-    "sangsoonam.vscode-extension-quick-grep",
-    "tamasfe.even-better-toml",
-    "thenuprojectcontributors.vscode-nushell-lang",
-    "tinkertrain.theme-panda",
-    "weijunyu.vscode-json-path"
-)
-
-$installed = -split (code --list-extensions)
-
-Write-Host "🔮 Installing VS Code extensions..." -ForegroundColor Yellow
-foreach ($ext in $vsCodeExtensions) {
-    if ($installed -contains $ext) {
-        Write-Host "✅ $($ext) is already installed" -ForegroundColor Green
-    } else {
-        code --install-extension $ext
     }
 }
